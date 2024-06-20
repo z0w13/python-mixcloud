5f746b2f6cedefa71d1104f24a4b4fd6f54ff982
Zowie <git@zowie.cx>
2024-06-20 21:33:43 +0200

    fix: we don't need to import pip

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
-from pip._internal.req import parse_requirements
-import setuptools
+from setuptools import setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
-with open('HISTORY.rst') as f:
+with open("HISTORY.rst") as f:
     history = f.read()
 
-setuptools.setup(name='mixcloud',
-                 version='0.4.0+dev',
-                 author='Etienne Millon',
-                 author_email='me@emillon.org',
-                 url="https://github.com/emillon/mixcloud",
-                 license='BSD',
-                 packages=['mixcloud'],
-                 install_requires=[
-                     'python-dateutil',
-                     'requests',
-                     'unidecode',
-                     'pyyaml',
-                 ],
-                 description='Bindings for the mixcloud.com API',
-                 long_description=readme + '\n\n' + history,
-                 classifiers=[
-                     'Development Status :: 3 - Alpha',
-                     'Intended Audience :: Developers',
-                     'License :: OSI Approved :: BSD License',
-                     'Operating System :: OS Independent',
-                     'Programming Language :: Python :: 2',
-                     'Programming Language :: Python :: 3',
-                     ],
-                 )
+setup(
+    name="mixcloud",
+    version="0.4.0+dev",
+    author="Etienne Millon",
+    author_email="me@emillon.org",
+    url="https://github.com/emillon/mixcloud",
+    license="BSD",
+    packages=["mixcloud"],
+    install_requires=[
+        "python-dateutil",
+        "requests",
+        "unidecode",
+        "pyyaml",
+    ],
+    description="Bindings for the mixcloud.com API",
+    long_description=readme + "\n\n" + history,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
